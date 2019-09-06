6c8ca73568aae29a41ccde98a9dc1d1110b08b20
dong o kim <otwmoo@gmail.com>
2019-09-06 16:12:58 +0900

    * 기초 스펙 정의 * [테스트][구현 중]휴식을 기록한다

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -9,6 +9,7 @@ describe('App', () => {
     container = document.createElement('div')
     document.body.appendChild(container)
   })
+
   test('basic', () => {
     act(() => {
       render(<App />, container)
@@ -17,6 +18,37 @@ describe('App', () => {
       container.querySelector('[data-test-id=btn-rest]').textContent
     ).toBe('휴식')
   })
+
+  test('휴식을 기록한다.', () => {
+    act(() => {
+      render(<App />, container)
+    })
+    const btnReset = container.querySelector('[data-test-id=btn-rest]')
+    act(() => {
+      btnReset.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    })
+  })
+
+  test('휴식을 다시 누르면 기록을 마친다.', () => {
+
+  })
+
+  test('일을 기록한다.', () => {
+
+  })
+
+  test('일을 다시 누르면 기록을 마친다.', () => {
+
+  })
+
+  test('일을 눌럿다가 휴식을 누르면 일은 마치고, 휴식을 시작한다.', () => {
+
+  })
+
+  test('휴식을 눌럿다가 일을 누르면 휴식은 마치고, 일을 시작한다.', () => {
+
+  })
+
   afterEach(() => {
     unmountComponentAtNode(container)
     container.remove()
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,60 @@
-import React from 'react';
-import { Button } from 'antd'
+import React, { useState } from 'react';
+import { Button, Slider } from 'antd'
+import 'antd/dist/antd.css';
 
-import moment from 'moment'
+import moment, { Moment } from 'moment'
 import './App.css';
 
-const ok = () => alert(moment().format('YYYY-MM-DD HH:mm:ss'))
+interface TaskType {
+  type: string;
+  color?: string;
+}
+
+interface Task {
+  start: Moment;
+  end?: Moment;
+  title?: string;
+  type?: TaskType;
+}
+
+const oneHour = 60
+const oneDay = 24 * oneHour
+
+const dayMarks = {
+  '00:00': {
+    label: '00:00'
+  }
+}
+
+const restType: TaskType = {
+  type: 'rest',
+  color: 'blue',
+}
+
+const current = () => Math.floor(moment.duration(moment().format('HH:mm:ss')).asMinutes())
+
 const App: React.FC = () => {
+  const [ tasks, setTask ] = useState<Task[]>([])
+
+  /**
+   * 새로운 task를 생성하여 tasks에 등록 한다.
+   */
+  const startRest = () => {
+    const newTask = {
+      start: moment(),
+      type: restType,
+    }
+    setTask([ ...tasks, newTask ])
+  }
   return (
     <div>
-      <Button onClick={ok} data-test-id="btn-rest">휴식</Button>
-      <Button onClick={ok} data-test-id="btn-work">work</Button>
+      <Button onClick={startRest} data-test-id="btn-rest">휴식</Button>
+      <Button onClick={startRest} data-test-id="btn-work">work</Button>
+      <div>
+        <Slider marks={dayMarks} defaultValue={0} min={0} max={oneDay}
+                step={ 1 }
+        />
+      </div>
     </div>
   );
 }
